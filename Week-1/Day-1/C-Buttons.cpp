ad7fb8f5fa88883a78906a29b6a80d7cdfe7131b
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2024-10-27 11:51:31 +0600

    week 1 Day 1 3 problems solution added

--- Week-1/Day-1/C-Buttons.cpp
+++ Week-1/Day-1/C-Buttons.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
-    int a , b; 
-    cin >> a >> b; 
 
+    int a, b;
+    cin >> a >> b;
 
-    if( a > b){
-        cout << a + (a-1) << "\n"; 
-    }  
-    else if(a < b) {
-        cout << b + (b-1) << "\n"; 
+    if (a > b)
+    {
+        cout << a + (a - 1) << "\n";
     }
-    else cout << a + b << "\n";
-    
+    else if (a < b)
+    {
+        cout << b + (b - 1) << "\n";
+    }
+    else
+    {
+        cout << a + b << "\n";
+    }
 
     return 0;
 }
