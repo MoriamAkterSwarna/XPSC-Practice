899724ee500fd9454b5bed1f5df7c3a23da209c5
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2024-11-09 17:50:05 +0600

    Week2 Day5 => solved 3 problems

---
+++ Week-2/Day-6/B_2_The_Strict_Teacher_Hard_Version.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define tst   \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+void firebird_solve()
+{
+    int cells_n, teachers_m, queries_q;
+    vector<int> teachers;
+    cin >> cells_n >> teachers_m >> queries_q;
+
+    for (int i = 0; i < teachers_m; i++)
+    {
+        int teacher;
+        cin >> teacher;
+        teachers.push_back(teacher);
+    }
+
+    sort(teachers.begin(), teachers.end());
+    while (queries_q--)
+    {
+        int query;
+        cin >> query;
+
+        if (query < teachers[0])
+        {
+            cout << teachers[0] - 1 << nl;
+        }
+        else if (query > teachers.back())
+        {
+            cout << cells_n - teachers.back() << nl;
+        }
+        else
+        {
+            auto it = lower_bound(teachers.begin(), teachers.end(), query);
+
+            int right = cells_n;
+            if (it != teachers.end())
+            {
+                right = *it;
+            }
+            int left = 1;
+            if (it != teachers.begin())
+            {
+                left = *(--it);
+            }
+
+            
+            cout << (right - left) / 2 << nl;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    tst
+    {
+        firebird_solve();
+    }
+
+    return 0;
+}
---
+++ Week-2/Day-6/C_Numbers_on_Whiteboard.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define tst   \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+void firebird_solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v;
+    vector<int> result;
+    for (int i = 1; i <= n; i++)
+    {
+        v.push_back(i);
+    }
+
+    // using for loop
+    for (int i = v.size(); i > 1; i--)
+    {
+
+        int summation = v[v.size() - 1] + v[v.size() - 2];
+        // cout << v[v.size() - 1] << " " << v[v.size() - 2] << nl;
+        result.push_back(v[v.size() - 1]);
+        result.push_back(v[v.size() - 2]);
+        v.pop_back();
+        v.pop_back();
+        if (summation % 2 == 1)
+        {
+            v.push_back(summation / 2 + 1);
+        }
+        else
+        {
+            v.push_back(summation / 2);
+        }
+    }
+
+    //  using while loop
+
+    // while (v.size() > 1)
+    // {
+    //     int summation = v[v.size() - 1] + v[v.size() - 2];
+    //     cout << v[v.size() - 1] << " " << v[v.size() - 2] << nl;
+    //     result.push_back(v[v.size() - 1]);
+    //     result.push_back(v[v.size() - 2]);
+
+    //     v.pop_back();
+    //     v.pop_back();
+    //     if (summation % 2 == 1)
+    //     {
+    //         v.push_back(summation / 2 + 1);
+    //     }
+    //     else
+    //     {
+    //         v.push_back(summation / 2);
+    //     }
+    // }
+
+    cout << v.back() << nl;
+    for (int i = 0; i < result.size(); i += 2)
+    {
+        cout << result[i] << " " << result[i + 1] << nl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    tst
+    {
+        firebird_solve();
+    }
+
+    return 0;
+}
